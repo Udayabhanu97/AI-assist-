4256721e0ac320670666e72a93bd62f0f1804be0
udayabhanu97 <udayabhanus65@gmail.com>
2026-03-14 14:07:04 +0530

    fix allowed hosts and middleware

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,8 +65,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-
-    'django.middleware.common.CommonMiddleware',
 
 ]
 
